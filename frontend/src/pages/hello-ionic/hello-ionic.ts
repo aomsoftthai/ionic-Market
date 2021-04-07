7296eff806bfa28688d4467c782817f8e3d2e910
aomsoftthai <apisit.p@softthai.com>
2021-04-07 05:13:03 +0100

    ADD STEP 1 | test import material | All

--- frontend/src/pages/hello-ionic/hello-ionic.ts
+++ frontend/src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor() {}
 
-  }
+  loading: boolean = false;
+
+  myForm: FormGroup;
 }
