515c3816ac4bd05db09370e5f05fba079d9a6f32
aomsoftthai <apisit.p@softthai.com>
2021-04-07 11:49:34 +0100

    ADD STEP 3 | Add page to chip | Home

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { AboutUsPage } from '../about-us/about-us';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
 export class HomePage {
 
-  chips: string[];
+  chips: Array<{ item: number, title?: string, page: any }>;
   cards: Array<{ item: number, title?: string, content?: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.chips = ["Electronic", "Chicken", "Food", "Smart-Phone", "National"];
+    this.chips = [
+      { item: 1, title: "About Us", page: AboutUsPage }
+    ];
 
     this.cards = [];
-    for(let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
       this.cards.push({
         item: i,
         title: "Title " + i,
@@ -29,6 +31,8 @@ export class HomePage {
 
   onClickChip(item) {
     console.log(`onClickChip`, item);
+
+    !!item.page && this.navCtrl.push(item.page);
   }
 
 }
