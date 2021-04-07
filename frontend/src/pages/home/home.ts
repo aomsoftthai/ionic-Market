aa5330ae9721aa8b0166ff9f5fb17bccd75657d9
aomsoftthai <apisit.p@softthai.com>
2021-04-07 10:54:23 +0100

    ADD NOT ANY CHANGE

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -7,11 +7,28 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
+  chips: string[];
+  cards: Array<{ item: number, title?: string, content?: string }>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.chips = ["Electronic", "Chicken", "Food", "Smart-Phone", "National"];
+
+    this.cards = [];
+    for(let i = 1; i <= 10; i++) {
+      this.cards.push({
+        item: i,
+        title: "Title " + i,
+        content: "Content " + i
+      });
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  onClickChip(item) {
+    console.log(`onClickChip`, item);
   }
 
 }
