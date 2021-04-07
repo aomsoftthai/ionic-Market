0509676b315a9ee96840af928161a50b15e03bbc
aomsoftthai <apisit.p@softthai.com>
2021-04-07 07:13:17 +0100

    WARNING will add | input | Login

--- frontend/src/pages/login/login.ts
+++ frontend/src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CommonNg } from '../../providers/providers';
 
 @Component({
   selector: 'page-login',
@@ -8,16 +9,23 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class LoginPage {
 
-  
   loading: boolean = false;
   myForm: FormGroup;
   sUserID: FormControl;
   sPassword: FormControl;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {}
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+    /* Validators Schema */
+    this.sUserID = this.fb.control('', Validators.required);
+    this.sPassword = this.fb.control('', Validators.required);
+
+    this.myForm = this.fb.group({ 'sUserID': this.sUserID, 'sPassword': this.sPassword });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+
+  onLogin() { }
 
 }
--- frontend/src/providers/common-ng/common-ng.ts
+++ frontend/src/providers/common-ng/common-ng.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import {
     ActionSheetController, ActionSheet, ActionSheetButton,
     AlertController, Alert, AlertButton, AlertOptions,
