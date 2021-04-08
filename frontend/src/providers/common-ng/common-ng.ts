493f07afb4dd8771f90a8d3b36e71e927ce50d3b
aomapisit <apisitprompha@gmail.com>
2021-04-08 13:31:13 +0700

    ADD STEP 1 | CommonNg

--- frontend/src/providers/common-ng/common-ng.ts
+++ frontend/src/providers/common-ng/common-ng.ts
@@ -4,7 +4,7 @@ import {
     AlertController, Alert, AlertButton, AlertOptions,
     LoadingController, Loading, LoadingOptions,
     ToastController, Toast, ToastOptions,
-    ModalController, Modal, 
+    ModalController, Modal,
     // ModalOptions,
     Platform
 } from 'ionic-angular';
@@ -13,13 +13,39 @@ import { AlertInputOptions } from 'ionic-angular/components/alert/alert-options'
 @Injectable()
 export class CommonNg {
     isCordova: boolean = false;
-    isiOS: boolean = false; 
+    isiOS: boolean = false;
     isAndroid: boolean = false;
-
 
     constructor(private actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController, private loadingCtrl: LoadingController, private modalCtrl: ModalController, private toastCtrl: ToastController, private platform: Platform) {
         this.isCordova = this.platform.is('cordova');
         this.isiOS = this.platform.is('ios');
         this.isAndroid = this.platform.is('android');
     }
+
+    platformReady() { return this.platform.ready() }
+
+    /*=====  ActionSheet Controller =====*/
+    getActionSheet(title: string, button: ActionSheetButton[]): ActionSheet {
+        let actionSheet = this.actionSheetCtrl.create({ title: title, buttons: button });
+        return actionSheet;
+    }
+
+    createActionSheetButton(text: string, hander: any, role?: string, icon?: string, cssClass?: string): ActionSheetButton {
+        let buttons: ActionSheetButton = {};
+        buttons.text = text;
+        buttons.handler = hander;
+
+        if (!this.isEmpty(role)) buttons.role = role;
+        if (!this.isEmpty(icon)) buttons.icon = icon;
+        if (!this.isEmpty(cssClass)) buttons.cssClass = cssClass;
+
+        return buttons;
+    }
+
+    /*=====  Alert Controller =====*/
+    
+
+    /*===== Utility Function  =====*/
+    isEmpty(obj): boolean { return (obj || '') == ''; }
+
 }
